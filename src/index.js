fa66a3cc32cff66ccfb8f25353e1c312b13a9bdc
Shirsha Sengupta <workofshirsha@gmail.com>
2023-07-03 16:26:04 +0530

    Environmental Variables Added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 
+const {PORT}= require("./config/serverConfig");
+
 
 const startAndSetupServer= async()=>{
 
     //create the express object
 
     const app=express();
-
-    const PORT= 3000;
 
     app.listen(PORT,()=>{
 
